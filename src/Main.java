ce01424654625b877a2cb4b612477c0306e74825
KetlynLuana <luanaketlyn80@gmail.com>
2026-03-13 13:06:16 -0300

    feat: :sparkles: Lista

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import model.Cliente;
+import model.Hamburguer;
+import model.Pedido;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> ingredientes = new
+    }
+}
---
+++ src/model/Pedido.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Pedido {
+
+
+    private Cliente cliente;
+    private Hamburguer hamburguer;
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Hamburguer getHamburguer() {
+        return hamburguer;
+    }
+
+    public void setHamburguer(Hamburguer hamburguer) {
+        this.hamburguer = hamburguer;
+    }
+
+    public Pedido(Cliente cliente, Hamburguer hamburguer) {
+        this.cliente = cliente;
+        this.hamburguer = hamburguer;
+
+        }
+    }
+
+
+
+
