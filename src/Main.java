90c47070e01c7e235eabb60e1de791df88194c83
KetlynLuana <luanaketlyn80@gmail.com>
2026-03-20 12:59:39 -0300

    feat: :sparkles: Aula do dia 20/03/26

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,55 @@ import model.Hamburguer;
 import model.Pedido;
 
 import java.util.ArrayList;
-import java.util.List;
 
+//METAS DE HOJE:
+//1 - Preço do Hamburguer
+//2- Remover Ingrediente
+//3- Adicionar Ingrediente
+//4- Calculo Total do Pedido
+model.Cliente;
+import model.Hamburguer;
+import model.Pedido;
+
+import java.util.ArrayList;
+im
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<String> ingredientes = new
+        ArrayList<String> ingredientesList = new ArrayList<>();
+        ingredientesList.add("pão");
+        ingredientesList.add("hamburguer");
+        ingredientesList.add("queijo");
+        ingredientesList.add("bacon");
+        ingredientesList.add("jiló");
+        ingredientesList.add("ovo");
+
+        //remover da Lista
+        //ingredienteList.remover("jiló");
+        //Sytem.out.println(ingredienteList);
+
+        //varrer Lista ( COM WHILE
+        //Integer cont = 0;
+        //Integer cont_ing= 0;
+        //while (cont < 6){
+        //     cont_ing += 1;
+        //     System.out.println(ingredienteList.get(cont));
+        //     cont = cont + 1;
+        //}
+        //System.out.println("A quantidade de ingredientes é: " + cont_i");
+
+        //Varrer Lista COM FOR
+        int cont_ing = 0;
+        for(int cont = 0 ; cont<6 ;cont++){
+            cont_ing += 1;
+            System.out.println(ingredienteList.get(cont));
+        }
+
+        Cliente cliente = new Cliente("Kelyn", "Luana");
+        Hamburguer simples = new Hamburguer(ingredientesList);
+
+        Pedido pedido1 = new Pedido(cliente, simples);
+
+        System.out.println(hamburguer.getingredientes());
     }
 }
